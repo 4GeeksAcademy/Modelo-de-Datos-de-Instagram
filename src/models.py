a43dbeee0d09dbe3ab92e6587950d8d9e67756d2
Jonas <jonas.backline@gmail.com>
2025-05-03 15:27:02 +0000

    1st commit users + posts

--- src/models.py
+++ src/models.py
@@ -1,19 +1,54 @@
+from eralchemy2 import render_er
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, DateTime, ForeignKey, Text
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    __tablename__ = 'users'
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            # do not serialize the password, it's a security breach
         }
+
+
+class Posts(db.Model):
+    __tablename__ = 'posts'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id'), nullable=False)
+    image_url: Mapped[str] = mapped_column(String(255), nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(), default=datetime.utcnow)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "image_url": self.image_url,
+            "content": self.content,
+            "created_at": self.created_at.isoformat()
+        }
+
+
+# Generar diagrama
+try:
+    render_er(db.Model, 'diagram.png')
+    print("Diagrama generado exitosamente")
+except Exception as e:
+    print("Hubo un problema generando el diagrama")
+    raise e
